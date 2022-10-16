323acf6455f893b097bd66e65c2202ed7061c518
Jordan Powers <jordanpowers1227@gmail.com>
2022-10-16 02:00:48 -0700

    .\process.py

--- process.py
+++ process.py
@@ -1,85 +1,127 @@
 import subprocess
 import sys
-import threading
-import queue
-import signal
 import time
+import argparse
+import shutil
+from datetime import datetime
+import os
 
 from pathlib import Path
 
 NUM_WORKERS = 4
 POLL_DELAY = 1
-DEST_PATH = R"\\192.168.1.204\Public\Shared Videos\tapes\mp4_split"
+DEST_PATH = R"\\192.168.1.204\Public\Shared Videos"
 
-input_dir = Path.cwd() / "input"
-worker_file = Path(__file__).parent / "worker.py"
+parser = argparse.ArgumentParser()
+parser.add_argument('input_dir')
+parser.add_argument('--intermediate-dir', default='')
+parser.add_argument('--done-dir', default='')
 
-assert worker_file.exists()
+args = parser.parse_args()
 
-if not input_dir.exists():
-    input_dir.mkdir()
+input_dir = Path(args.input_dir)
+if not input_dir.is_dir():
+    print('Invalid input_dir!')
+    exit(1)
 
-interrupted = False
+if args.intermediate_dir:
+    int_dir = Path(args.intermediate_dir)
+else:
+    int_dir = Path.cwd() / 'intermediates'
 
-emergency_escape = 3
+if args.done_dir:
+    done_dir = Path(args.done_dir)
+else:
+    done_dir = Path.cwd() / 'done'
 
-def signal_handler(sig, frame):
-    global interrupted, emergency_escape
-    print("Interrupting jobs...")
-    interrupted = True
+if int_dir.is_dir():
+    shutil.rmtree(int_dir)
+int_dir.mkdir()
 
-    emergency_escape -= 1
+if done_dir.is_dir():
+    print("Done dir already exists!!")
+    exit(1)
+done_dir.mkdir()
 
-    if emergency_escape == 0:
-        sys.exit(1)
+files = list(input_dir.glob("*/*.avi"))
 
-signal.signal(signal.SIGINT, signal_handler)
+print(f"\x1b[92mCopy originals\x1b[37m")
+cmd = [
+    "robocopy",
+    str(input_dir),
+    DEST_PATH + '\\tapes_avi',
+    "/j", "/s",
+    "/mt:8",
+    "/nc",
+    "/compress"
+]
+completion = subprocess.run(cmd)
+if completion.returncode >= 8:
+    raise subprocess.CalledProcessError(completion.returncode, cmd)
 
-jobs = queue.SimpleQueue()
+for i, file in enumerate(files):
+    print(f"\x1b[92m({i:03d}/{len(files):03d})\x1b[37m: {file.name}")
+    subdir = file.parent
+    if subdir != input_dir:
+        assert subdir.parent == input_dir
+        int_subdir = int_dir / subdir.name
+        if not int_subdir.exists():
+            int_subdir.mkdir()
 
-def run_worker():
-    while True:
-        if not interrupted:
-            try:
-                inpath = jobs.get(block=True, timeout=POLL_DELAY)
-            except queue.Empty:
-                continue
-        else:
-            try:
-                inpath = jobs.get_nowait()
-            except queue.Empty:
-                return
+        done_subdir = done_dir / subdir.name
+        if not done_subdir.exists():
+            done_subdir.mkdir()
 
-        subprocess.run([sys.executable, str(worker_file), str(inpath), DEST_PATH], creationflags=subprocess.CREATE_NEW_CONSOLE).check_returncode()
+        int_file = int_subdir / (file.stem + '.mp4')
+        done_file = done_subdir / file.name
+    else:
+        int_file = int_dir / (file.stem + '.mp4')
 
-        inpath.unlink()
+    if int_file.exists():
+        raise ValueError('Conflicting intermediate files!')
 
+    record_date = file.name.split('.')[-3]
+    record_date = datetime.strptime(record_date, '%y-%m-%d_%H-%M-%S')
 
-workers = [threading.Thread(target=run_worker, name=f"Worker {i}") for i in range(NUM_WORKERS)]
-for worker in workers:
-    worker.start()
+    # TRANSCODE
+    cmd = [
+        'ffmpeg',
+        '-v', 'warning',
+        '-stats',
+        '-i', str(file),
+        '-filter:v', 'yadif=mode=1',
+        '-pix_fmt', 'yuv420p',
+        '-c:v', 'libx264',
+        '-preset', 'slow',
+        '-crf', '18',
+        '-c:a', 'aac',
+        '-b:a', '128k',
+        '-metadata', f'date={record_date.strftime("%Y-%m-%d")}',
+        str(int_file)
+    ]
+    subprocess.run(cmd).check_returncode()
 
-known_files = set()
+    # SET TIME
+    os.utime(str(int_file), (record_date.timestamp(), record_date.timestamp()))
 
-last_size = 0
+    # UPLOAD
+    cmd = [
+        "robocopy",
+        str(int_dir),
+        DEST_PATH,
+        "/j", "/s",
+        "/nc",
+        "/mov",
+        "/compress"
+    ]
+    completion = subprocess.run(cmd)
+    if completion.returncode >= 8:
+        raise subprocess.CalledProcessError(completion.returncode, cmd)
 
-while not interrupted:
-    for file in input_dir.glob("**/*.avi"):
-        if file.name not in known_files:
-            print(f"Discovered {file.name}")
-            jobs.put(file)
-            known_files.add(file.name)
+    assert not int_file.exists()
 
-    size = jobs.qsize()
-    if (size > 0 or last_size > 0) and size != last_size:
-        print(f"Pending jobs: {size}")
-        last_size = size
+    # MOVE
+    assert not done_file.exists()
+    file.rename(done_file)
 
-    time.sleep(POLL_DELAY)
-
-for worker in workers:
-    print(f"Waiting for {worker.name}...", end='')
-    sys.stdout.flush()
-    worker.join()
-    print("Done")
 
